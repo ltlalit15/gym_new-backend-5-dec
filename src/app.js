a68d9b5d05b3a8b1f484c6281ef36823000dae75
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-05 20:49:11 +0530

    sdad

--- src/app.js
+++ src/app.js
@@ -32,7 +32,8 @@ app.use(
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "http://localhost:5175"
+      "http://localhost:5175",
+      "https://gym-latest-new.netlify.app"
 
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
