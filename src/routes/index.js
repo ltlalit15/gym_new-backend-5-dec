1824c3657e34a582118fa4ec78955d9a0c588b71
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-05 20:50:17 +0530

    sdad

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,7 +31,7 @@ import MemberPlan from "../modules/memberplan/memberPlan.routes.js";
 import SalaryRoutes from "../modules/salary/salary.routes.js";
 import houseKeepingRoutes from "../modules/houseKeppingtask/housekeepingtask.routes.js";
 import staffAttendanceRoutes from "../modules/staffAttendance/staffAttendance.routes.js";
-import housekeepingDashboardRoutes from "../modules/dashboard/housekeepingdashboard.routes.js";
+// import housekeepingDashboardRoutes from "../modules/dashboard/housekeepingdashboard.routes.js";
 import generaltrainerRoutes from "../modules/generaltrainer/generalTrainer.routes.js";
 import memberAttendenceRoutes from "../modules/memberattendence/attendence.routes.js";
 import shiftRoutes from "../modules/shift/shift.routes.js";
