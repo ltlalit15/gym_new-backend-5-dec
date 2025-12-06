9a37a14fc6de9ee26779fa8c3d6a27fefae87294
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-06 11:25:19 +0530

    sadda

--- src/config/db.js
+++ src/config/db.js
@@ -1,34 +1,3 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-dotenv.config();
-
-// Create a **Promise Pool directly**
-export const pool = mysql
-  .createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "gym_db",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  })
-  .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
-
-// Test MySQL connection
-pool.getConnection()
-  .then((connection) => {
-    console.log("✅ MySQL connected successfully!");
-    connection.release();
-  })
-  .catch((err) => {
-    console.error("❌ MySQL connection failed:", err.message);
-  });
-
-
-
-// live database
 // import mysql from 'mysql2';
 // import dotenv from 'dotenv';
 // dotenv.config();
@@ -36,11 +5,11 @@ pool.getConnection()
 // // Create a **Promise Pool directly**
 // export const pool = mysql
 //   .createPool({
-//     host: "switchback.proxy.rlwy.net",
+//     host: "localhost",
 //     user: "root",
-//     password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
-//     database: "railway",
-//     port: 35602,
+//     password: "",
+//     database: "gym_db",
+//     port: 3306,
 //     waitForConnections: true,
 //     connectionLimit: 10,
 //     queueLimit: 0,
@@ -56,3 +25,34 @@ pool.getConnection()
 //   .catch((err) => {
 //     console.error("❌ MySQL connection failed:", err.message);
 //   });
+
+
+
+live database
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Create a **Promise Pool directly**
+export const pool = mysql
+  .createPool({
+    host: "switchback.proxy.rlwy.net",
+    user: "root",
+    password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
+    database: "railway",
+    port: 35602,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
+
+// Test MySQL connection
+pool.getConnection()
+  .then((connection) => {
+    console.log("✅ MySQL connected successfully!");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("❌ MySQL connection failed:", err.message);
+  });
