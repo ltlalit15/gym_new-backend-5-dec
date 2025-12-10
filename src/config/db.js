8b264f689c0d9ed7ebe9780997f457ae0e94ff0b
Kapil110248 <kapilpatidarkapil0812@gmail.com>
2025-12-10 15:25:12 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -28,58 +28,27 @@
 //   });
 
 // live database
-import mysql from "mysql2";
-import dotenv from "dotenv";
-dotenv.config();
-
-// Create a **Promise Pool directly**
-export const pool = mysql
-  .createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "gym_db",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  })
-  .promise(); // 🔥 THIS MAKES pool.query() RETURN A PROMISE
-
-// Test MySQL connection
-pool
-  .getConnection()
-  .then((connection) => {
-    console.log("✅ MySQL connected successfully!");
-    connection.release();
-  })
-  .catch((err) => {
-    console.error("❌ MySQL connection failed:", err.message);
-  });
-
-
-
-// // live database
-// import mysql from 'mysql2';
-// import dotenv from 'dotenv';
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
 // dotenv.config();
 
 // // Create a **Promise Pool directly**
 // export const pool = mysql
 //   .createPool({
-//     host: "switchback.proxy.rlwy.net",
+//     host: "localhost",
 //     user: "root",
-//     password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
-//     database: "railway",
-//     port: 35602,
+//     password: "",
+//     database: "gym_db",
+//     port: 3306,
 //     waitForConnections: true,
 //     connectionLimit: 10,
 //     queueLimit: 0,
 //   })
-//   .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
+//   .promise(); // 🔥 THIS MAKES pool.query() RETURN A PROMISE
 
 // // Test MySQL connection
-// pool.getConnection()
+// pool
+//   .getConnection()
 //   .then((connection) => {
 //     console.log("✅ MySQL connected successfully!");
 //     connection.release();
@@ -87,3 +56,34 @@ pool
 //   .catch((err) => {
 //     console.error("❌ MySQL connection failed:", err.message);
 //   });
+
+
+
+// live database
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Create a **Promise Pool directly**
+export const pool = mysql
+  .createPool({
+    host: "switchback.proxy.rlwy.net",
+    user: "root",
+    password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
+    database: "railway",
+    port: 35602,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
+
+// Test MySQL connection
+pool.getConnection()
+  .then((connection) => {
+    console.log("✅ MySQL connected successfully!");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("❌ MySQL connection failed:", err.message);
+  });
