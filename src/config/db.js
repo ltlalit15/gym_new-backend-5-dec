6becc3f4c81f41220f1a4f4b2a0cf10d6c0cb98e
Kapil110248 <kapilpatidarkapil0812@gmail.com>
2025-12-12 18:23:00 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -59,31 +59,31 @@
 
 
 
-// // // // live database
-// import mysql from 'mysql2';
-// import dotenv from 'dotenv';
-// dotenv.config();
+// // // live database
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// // Create a **Promise Pool directly**
-// export const pool = mysql
-//   .createPool({
-//     host: "switchback.proxy.rlwy.net",
-//     user: "root",
-//     password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
-//     database: "railway",
-//     port: 35602,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//   })
-//   .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
+// Create a **Promise Pool directly**
+export const pool = mysql
+  .createPool({
+    host: "switchback.proxy.rlwy.net",
+    user: "root",
+    password: "LYEPuGdFNazTUxSFwrZilcKIAOlztDYo",
+    database: "railway",
+    port: 35602,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();  // 🔥 THIS MAKES pool.query() RETURN A PROMISE
 
-// // Test MySQL connection
-// pool.getConnection()
-//   .then((connection) => {
-//     console.log("✅ MySQL connected successfully!");
-//     connection.release();
-//   })
-//   .catch((err) => {
-//     console.error("❌ MySQL connection failed:", err.message);
-//   });
+// Test MySQL connection
+pool.getConnection()
+  .then((connection) => {
+    console.log("✅ MySQL connected successfully!");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("❌ MySQL connection failed:", err.message);
+  });
